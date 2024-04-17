e0717326b72bc37a2048df11d1ed056be3a9354d
Jonathon <jonathon.teasdale@gmail.com>
2024-04-18 00:14:56 +1000

    Initial client side error handling

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -8,12 +8,17 @@ import { Route, Routes } from 'react-router-dom';
 import HomePage from '../../features/home/HomePage';
 import ActivityForm from '../../features/activities/form/ActivityForm';
 import ActivityDetails from '../../features/activities/details/ActivityDetails';
+import TestErrors from '../../features/errors/TestError';
+import { ToastContainer } from 'react-toastify';
+import NotFound from '../../features/errors/NotFound';
 
 
 function App() {
 
   return (
     <Fragment>
+      <ToastContainer position='bottom-right' hideProgressBar />
+      
       <Routes>
         <Route path='/' element={<HomePage />} />
 
@@ -30,7 +35,10 @@ function App() {
                 <Route path='/activities/:id' element={<ActivityDetails />} />
 
                 <Route path='/createActivity' element={<ActivityForm key="create" />} />
-                <Route path='/manage/:id' element={<ActivityForm  key="edit" />} />                
+                <Route path='/manage/:id' element={<ActivityForm  key="edit" />} />    
+                <Route path='/errors' element={<TestErrors />} />
+
+                <Route path="*" element={<NotFound />} />
               </Routes>
             </Container>
 
